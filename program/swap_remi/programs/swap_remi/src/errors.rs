1e26d0f337a77a26f506fcee746f8d78e3e6ac54
NguyenLoi851 <nguyensyloi851@gmail.com>
2023-04-27 23:59:59 +0700

    feat/deposit-and-swap-happy-case

--- program/swap_remi/programs/swap_remi/src/errors.rs
+++ program/swap_remi/programs/swap_remi/src/errors.rs
@@ -4,4 +4,8 @@ use anchor_lang::error_code;
 pub enum SwapError {
     #[msg("Invalid radio")]
     InvalidRadio,
+    #[msg("Invalid swap direction")]
+    InvalidSwapDirection,
+    #[msg("Not enough liquidity to swap")]
+    NotEnoughLiquidity
 }
