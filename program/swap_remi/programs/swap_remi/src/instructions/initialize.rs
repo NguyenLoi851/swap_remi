fe2456fc53aba76d480a6b1c2ed0a177142bb28e
NguyenLoi851 <nguyensyloi851@gmail.com>
2023-04-28 20:34:33 +0700

    transfer SOL to pool state account

--- program/swap_remi/programs/swap_remi/src/instructions/initialize.rs
+++ program/swap_remi/programs/swap_remi/src/instructions/initialize.rs
@@ -5,10 +5,8 @@ use anchor_spl::token::{Mint, Token, TokenAccount};
 
 pub fn initialize(ctx: Context<Initialize>, price: u64) -> Result<()> {
     let state = &mut ctx.accounts.pool_state;
-    state.mint_acc_token_0 = ctx.accounts.mint_acc_token_0.key().clone();
     state.mint_acc_token_1 = ctx.accounts.mint_acc_token_1.key().clone();
     state.price = price;
-    state.pool_wallet_token_0 = ctx.accounts.pool_wallet_token_0.key().clone();
     state.pool_wallet_token_1 = ctx.accounts.pool_wallet_token_1.key().clone();
     state.bump = *ctx.bumps.get("pool_state").unwrap();
     Ok(())
@@ -19,7 +17,7 @@ pub struct Initialize<'info> {
     #[account(
         init,
         payer = sender,
-        seeds = [b"state".as_ref(), mint_acc_token_0.key().as_ref(), mint_acc_token_1.key().as_ref()],
+        seeds = [b"state".as_ref(), mint_acc_token_1.key().as_ref()],
         bump,
         space = PoolState::LEN
     )]
@@ -28,22 +26,7 @@ pub struct Initialize<'info> {
     #[account(mut)]
     sender: Signer<'info>,
 
-    #[account(
-        constraint = mint_acc_token_0.key() < mint_acc_token_1.key()
-    )]
-    mint_acc_token_0: Account<'info, Mint>,
-
     mint_acc_token_1: Account<'info, Mint>,
-
-    #[account(
-        init,
-        payer = sender,
-        seeds = [b"wallet".as_ref(), mint_acc_token_0.key().as_ref()],
-        bump,
-        token::mint = mint_acc_token_0,
-        token::authority = pool_state
-    )]
-    pool_wallet_token_0: Account<'info, TokenAccount>,
 
     #[account(
         init,
--- program/swap_remi/programs/swap_remi/src/instructions/swap.rs
+++ program/swap_remi/programs/swap_remi/src/instructions/swap.rs
@@ -1,77 +1,105 @@
-use anchor_lang::prelude::*;
-use anchor_spl::token::{TokenAccount, Token, Transfer};
+use anchor_lang::{prelude::*, system_program};
+use anchor_spl::token::{Token, TokenAccount, Transfer};
 
-use crate::state::PoolState;
 use crate::errors::SwapError;
+use crate::state::PoolState;
 
 // If amount_0 > 0, swap from token_0 to token_1
 // and vice versa
 // 1 (token_0) = 1 (token_1) * price
-pub fn swap(
-    ctx: Context<Swap>,
-    amount_0: u64,
-    amount_1: u64
-) -> Result<()> {
+pub fn swap(ctx: Context<Swap>, amount_0: u64, amount_1: u64) -> Result<()> {
     // Check valid swap direction
     if (amount_0 == 0 && amount_1 == 0) || (amount_0 > 0 && amount_1 > 0) {
-        return Err(SwapError::InvalidSwapDirection.into())
+        return Err(SwapError::InvalidSwapDirection.into());
     }
     let pool_state = &ctx.accounts.pool_state;
 
     // Set swap direction
     let price = pool_state.price;
-    let amount_in = if amount_0 > 0 {amount_0} else {amount_1};
-    let amount_out = if amount_in == amount_0 {amount_in * price} else {amount_in / price};
+    let amount_in = if amount_0 > 0 { amount_0 } else { amount_1 };
+    let amount_out = if amount_in == amount_0 {
+        amount_in * price
+    } else {
+        amount_in / price
+    };
 
-    let token_acc_in = if amount_in == amount_0 {ctx.accounts.token_acc_0.clone()} else {ctx.accounts.token_acc_1.clone()};
-    let token_acc_out = if amount_in == amount_0 {ctx.accounts.token_acc_1.clone()} else {ctx.accounts.token_acc_0.clone()};
+    // swap from SOL to token
+    if amount_in == amount_0 {
+        // sender transfer SOL to pool_state
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.sender.to_account_info(),
+                to: ctx.accounts.pool_state.to_account_info(),
+            },
+        );
 
-    let pool_wallet_token_in = if amount_in == amount_0 {ctx.accounts.pool_wallet_token_0.clone()} else {ctx.accounts.pool_wallet_token_1.clone()};
-    let pool_wallet_token_out = if amount_in == amount_0 {ctx.accounts.pool_wallet_token_1.clone()} else {ctx.accounts.pool_wallet_token_0.clone()};
+        system_program::transfer(cpi_ctx, amount_in)?;
 
-    // Check emoungh amount to swap
-    if amount_out > pool_wallet_token_out.amount {
-        return Err(SwapError::NotEnoughLiquidity.into())
+        // pool transfer token to sender
+        let transfer_instruction = Transfer {
+            from: ctx.accounts.pool_wallet_token_1.to_account_info(),
+            to: ctx.accounts.token_acc_1.to_account_info(),
+            authority: pool_state.to_account_info(),
+        };
+
+        let bump_vector = pool_state.bump.to_le_bytes();
+
+        let signer_seeds = vec![
+            b"state".as_ref(),
+            pool_state.mint_acc_token_1.as_ref(),
+            bump_vector.as_ref(),
+        ];
+
+        let signer_seeds_vector = vec![signer_seeds.as_slice()];
+
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_instruction,
+            signer_seeds_vector.as_slice(),
+        );
+
+        anchor_spl::token::transfer(cpi_ctx, amount_out)?;
     }
-    // ============ transfer token in ============
-    let transfer_instruction = Transfer{
-        from: token_acc_in.to_account_info(),
-        to: pool_wallet_token_in.to_account_info(),
-        authority: ctx.accounts.sender.to_account_info()
-    };
+    // swap from token to SOL
+    else {
+        // sender transfer token to pool_state
+        let transfer_instruction = Transfer {
+            from: ctx.accounts.token_acc_1.to_account_info(),
+            to: ctx.accounts.pool_wallet_token_1.to_account_info(),
+            authority: ctx.accounts.sender.to_account_info(),
+        };
 
-    let cpi_ctx = CpiContext::new(
-        ctx.accounts.token_program.to_account_info(),
-        transfer_instruction
-    );
+        let cpi_ctx = CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            transfer_instruction,
+        );
 
-    anchor_spl::token::transfer(cpi_ctx, amount_in)?;
+        anchor_spl::token::transfer(cpi_ctx, amount_in)?;
 
-    // ============ transfer token out ============
-    let transfer_instruction = Transfer{
-        from: pool_wallet_token_out.to_account_info(),
-        to: token_acc_out.to_account_info(),
-        authority: pool_state.to_account_info()
-    };
+        // pool transfer SOL to sender
 
-    let bump_vector = pool_state.bump.to_le_bytes();
+        let bump_vector = pool_state.bump.to_le_bytes();
 
-    let signer_seeds = vec![
-        b"state".as_ref(),
-        pool_state.mint_acc_token_0.as_ref(),
-        pool_state.mint_acc_token_1.as_ref(),
-        bump_vector.as_ref(),
-    ];
+        let signer_seeds = vec![
+            b"state".as_ref(),
+            pool_state.mint_acc_token_1.as_ref(),
+            bump_vector.as_ref(),
+        ];
+        
+        let signer_seeds_vector = vec![signer_seeds.as_slice()];
 
-    let signer_seeds_vector = vec![signer_seeds.as_slice()];
+        let cpi_ctx = CpiContext::new_with_signer(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.pool_state.to_account_info(),
+                to: ctx.accounts.sender.to_account_info(),
+            },
+            signer_seeds_vector.as_slice(),
+        );
 
-    let cpi_ctx = CpiContext::new_with_signer(
-        ctx.accounts.token_program.to_account_info(), 
-        transfer_instruction, 
-        signer_seeds_vector.as_slice()
-    );
-
-    anchor_spl::token::transfer(cpi_ctx, amount_out)?;
+        system_program::transfer(cpi_ctx, amount_out)?;
+    }
 
     Ok(())
 }
@@ -81,25 +109,19 @@ pub struct Swap<'info> {
     #[account(mut)]
     sender: Signer<'info>,
 
-    pool_state: Account<'info, PoolState>,
-
     #[account(mut)]
-    token_acc_0: Account<'info, TokenAccount>,
+    pool_state: Account<'info, PoolState>,
 
     #[account(mut)]
     token_acc_1: Account<'info, TokenAccount>,
 
     #[account(
         mut,
-        constraint = pool_wallet_token_0.key() == pool_state.pool_wallet_token_0
-    )]
-    pool_wallet_token_0: Account<'info, TokenAccount>,
-
-    #[account(
-        mut,
         constraint = pool_wallet_token_1.key() == pool_state.pool_wallet_token_1
     )]
     pool_wallet_token_1: Account<'info, TokenAccount>,
-    
+
     token_program: Program<'info, Token>,
+
+    system_program: Program<'info, System>,
 }
