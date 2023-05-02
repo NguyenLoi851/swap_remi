4431e6f133dc5f00d34c9d2911ebb32ce95a5577
NguyenLoi851 <nguyensyloi851@gmail.com>
2023-05-02 21:00:28 +0700

    rename title

--- fe/swap-remi/src/pages/index.tsx
+++ fe/swap-remi/src/pages/index.tsx
@@ -6,10 +6,10 @@ const Home: NextPage = (props) => {
   return (
     <div>
       <Head>
-        <title>Solana Scaffold</title>
+        <title>Solana Swap Remi</title>
         <meta
           name="description"
-          content="Solana Scaffold"
+          content="Solana Swap Remi"
         />
       </Head>
       <HomeView />
