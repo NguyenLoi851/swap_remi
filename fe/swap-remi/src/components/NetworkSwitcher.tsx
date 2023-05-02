b78019dfb169f0a6630c261e1ef4a8655d1d14bd
NguyenLoi851 <nguyensyloi851@gmail.com>
2023-05-02 18:05:33 +0700

    UI-add liquidity and swap

---
+++ fe/swap-remi/src/components/NetworkSwitcher.tsx
@@ -0,0 +1,34 @@
+import { FC, useEffect } from 'react';
+import dynamic from 'next/dynamic';
+import { useNetworkConfiguration } from '../contexts/NetworkConfigurationProvider';
+
+const NetworkSwitcher: FC = () => {
+  const { networkConfiguration, setNetworkConfiguration } = useNetworkConfiguration();
+
+  console.log(networkConfiguration);
+
+  useEffect(() => {
+    setNetworkConfiguration('testnet');
+  }, [])
+
+  return (
+    <div className='m-5'>
+      <label className="cursor-pointer label">
+        <a>Network {networkConfiguration}</a>
+        {/* <select             
+        value={networkConfiguration}
+        onChange={(e) => setNetworkConfiguration(e.target.value)} 
+        className="select max-w-xs"
+      >
+        <option value="mainnet-beta">mainnet</option>
+        <option value="testnet">testnet</option>
+        <option value="devnet">devnet</option>
+      </select> */}
+      </label>
+    </div>
+  );
+};
+
+export default dynamic(() => Promise.resolve(NetworkSwitcher), {
+  ssr: false
+})
